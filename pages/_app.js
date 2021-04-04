98ed92a0908dd1310eb871950dfd48d7b807dcac
Guido Nasini <gnasini.ds@gmail.com>
2021-04-04 20:33:48 -0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import NProgress from "nprogress";
+import Layout from "../components/Layout";
+import Router from "next/router";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+
+Router.events.on("routeChangeComplete", () => NProgress.done());
+
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
