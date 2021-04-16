fe5262904c28ffb88c8660195840e9d79a3432a9
Guido Nasini <gnasini.ds@gmail.com>
2021-04-15 21:46:34 -0300

    Finished contact prototype

--- components/Contact.js
+++ components/Contact.js
@@ -1,16 +1,34 @@
+import Image from "next/image";
 import { useState } from "react";
 import styled from "styled-components";
+
+const Title = styled.h1`
+  color: var(--darkPrimary);
+  font-weight: 700;
+  font-size: 6.4rem;
+  align-self: center;
+  justify-content: center;
+  padding: 0 15.5rem;
+  span {
+    color: #fff;
+  }
+`;
 
 const ContactWrapper = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  padding: 0 15.5rem;
+  margin: 0 15.5rem;
   align-items: center;
+
+  border: 2px solid var(--darkPrimary);
 
   .div-img-left {
     display: flex;
-    flex: 1;
+    flex: 0.5;
+    padding: 3rem;
+    align-items: center;
+    justify-content: center;
   }
 
   .div-content-right {
@@ -18,26 +36,29 @@ const ContactWrapper = styled.section`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 2rem;
+    padding: 4rem;
+    flex: 1;
+    width: 100%;
+    border-left: 2px solid var(--darkPrimary);
 
-    label {
-      display: flex;
+    form {
       width: 100%;
-      margin-bottom: 3rem;
-      justify-content: center;
-
-      input {
-        outline: none;
-        border: none;
-        height: 15px;
-        border: 2px solid var(--darkPrimary);
-        background: transparent;
-        font-family: "Roboto Mono";
+      label {
         color: #fff;
-        padding-left: 0.5rem;
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 3rem;
+        justify-content: center;
 
-        input > placeholder {
+        input {
+          outline: none;
+          border: none;
+          height: 25px;
+          border: 2px solid var(--darkPrimary);
+          background: transparent;
+          font-family: "Roboto Mono";
           color: #fff;
+          padding-left: 0.5rem;
         }
       }
     }
@@ -48,9 +69,25 @@ const ContactWrapper = styled.section`
       width: 100%;
       justify-content: center;
       align-items: center;
-      color: var(--darkPrimary);
+      color: #fff;
       border: 2px solid var(--darkPrimary);
+      background: transparent;
+      cursor: pointer;
+      height: 25px;
+      padding: 0;
+      margin: 0;
     }
+  }
+
+  .text-area {
+    outline: none;
+    border: none;
+    height: 100px;
+    border: 2px solid var(--darkPrimary);
+    background: transparent;
+    font-family: "Roboto Mono";
+    color: #fff;
+    padding-left: 0.5rem;
   }
 `;
 
@@ -73,43 +110,52 @@ const Contact = () => {
   const { email, subject, body } = inputs;
 
   return (
-    <ContactWrapper>
-      <div className="div-img-left">
-        <h1>Placeholder</h1>
-      </div>
-      <div className="div-content-right">
-        <form>
-          <label htmlFor="email">
-            <input
-              type="text"
-              name="email"
-              placeholder="Email Address"
-              value={email}
-              onChange={handleChange}
-            ></input>
-          </label>
-          <label htmlFor="subject">
-            <input
-              type="text"
-              name="subject"
-              placeholder="Subject"
-              value={subject}
-              onChange={handleChange}
-            ></input>
-          </label>
-          <label htmlFor="body">
-            <input
-              type="textarea"
-              name="body"
-              placeholder="Message"
-              value={body}
-              onChange={handleChange}
-            ></input>
-          </label>
-        </form>
-        <button type="submit">SEND</button>
-      </div>
-    </ContactWrapper>
+    <>
+      <Title>
+        0.4.<span>Contact</span>
+      </Title>
+      <ContactWrapper>
+        <div className="div-img-left">
+          <Image src="/assets/dev.svg" alt="planet" width={300} height={300} />
+        </div>
+        <div className="div-content-right">
+          <form>
+            <label htmlFor="email">
+              Email
+              <input
+                type="text"
+                name="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={handleChange}
+              ></input>
+            </label>
+            <label htmlFor="subject">
+              Subject
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                value={subject}
+                onChange={handleChange}
+              ></input>
+            </label>
+            <label htmlFor="body">
+              Body
+              <textarea
+                className="text-area"
+                type="textarea"
+                name="body"
+                placeholder="Message"
+                value={body}
+                onChange={handleChange}
+              ></textarea>
+            </label>
+          </form>
+          <button type="submit">SEND</button>
+        </div>
+      </ContactWrapper>
+    </>
   );
 };
 
